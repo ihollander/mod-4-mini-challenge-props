42727687216702e8e5e2aa752f159a2a4221e2cc
kelseyjdickson <kelseymedved@gmail.com>
2020-08-24 17:50:29 -0400

    with the help of others

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import React from 'react'
 // function component (arrow fn syntax)
 const Header = props => {
   // TODO: find a way to get the spiceCount passed down as a prop to this component
+  
+  
   return (
     <header>
       <h1>The Spice Store</h1>
--- src/components/SpiceList.js
+++ src/components/SpiceList.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import SpiceItem from './SpiceItem.js'
 // TODO: import the SpiceItem component here
 
 // class component
 class SpiceList extends React.Component {
 
   renderSpices() {
+
     // TODO: update this function to return an array of SpiceItem components
     // If you get an error message about a 'key' prop, make sure to fix it before submitting your code!
+    return this.props.spices.map(spice => <SpiceItem key={spice.id} title={spice.title} image={spice.image} description={spice.description}notes={spice.notes}/>)
   }
 
   render() {
