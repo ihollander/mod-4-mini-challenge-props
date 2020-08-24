6ee0058efc25db33faf34a5ddc3433cda167d2e8
tracycollins <tc@threeceemedia.com>
2020-08-24 15:43:45 -0400

    Done

--- src/components/SpiceItem.js
+++ src/components/SpiceItem.js
@@ -3,11 +3,12 @@ import React from 'react'
 class SpiceItem extends React.Component {
 
   render() {
+    console.log(this.props)
     return (
       <div className="spice-item">
-        <img src={null /* replace me with an image url */} alt={"replace me with a TITLE"} />
+        <img src={this.props.spice.image} alt={this.props.spice.title} />
         <div className="details">
-          <h2>{"replace me with a TITLE"}</h2>
+          <h2>{this.props.title}</h2>
           <p>{"replace me with a DESCRIPTION"}</p>
           <em>{"replace me with NOTES"}</em>
         </div>
--- src/components/SpiceList.js
+++ src/components/SpiceList.js
@@ -1,19 +1,19 @@
 import React from 'react'
-// TODO: import the SpiceItem component here
+import SpiceItem from './SpiceItem';
 
 // class component
 class SpiceList extends React.Component {
 
-  renderSpices() {
-    // TODO: update this function to return an array of SpiceItem components
-    // If you get an error message about a 'key' prop, make sure to fix it before submitting your code!
+  renderSpices = (spices) => {
+    return (
+      spices.map(spice => <SpiceItem spice={spice} />)
+    )
   }
 
   render() {
-    console.log("SpiceList props:", this.props.spices)
     return (
       <section className="spice-list">
-        {this.renderSpices()}
+        {this.renderSpices(this.props.spices)}
       </section>
     )
   }
