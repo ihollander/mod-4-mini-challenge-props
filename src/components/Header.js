51dd17906c26d650d98d80bc37c0d41239d6924a
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-24 15:40:08 -0400

    Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 // function component (arrow fn syntax)
 const Header = props => {
-  // TODO: find a way to get the spiceCount passed down as a prop to this component
   return (
     <header>
       <h1>The Spice Store</h1>
